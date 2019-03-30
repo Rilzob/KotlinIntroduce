d2124fb713aae515e7ff660817ffd49a814ae13d
Rilzob <watermirrorsir@163.com>
2019-03-30 14:52:58 +0800

    KotlinHello

---
+++ app/src/main/java/com/rilzob/kotlinhello/HelloActivity.kt
@@ -0,0 +1,36 @@
+package com.rilzob.kotlinhello
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+class HelloActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_hello)
+
+//        val colorTable = listOf("#ff0000", "#00ff00", "#0000ff", "#ffff00", "#00ffff", "#ff00ff")
+//        val label = findViewById<TextView>(R.id.label)
+//        label.setOnClickListener{ view ->
+//            val randomIndex = (Math.random() * colorTable.size).toInt()
+//            view.setBackgroundColor(Color.parseColor(colorTable[randomIndex]))
+//        }
+
+        val colorTable = listOf("#ff0000", "#00ff00", "#0000ff", "#ffff00", "#00ffff", "#ff00ff")
+
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                this.onClick {
+                    val randomIndex = (Math.random() * colorTable.size).toInt()
+                    setBackgroundColor(Color.parseColor(colorTable[randomIndex]))
+                    toast("Hello, ${name.text}! with color ${colorTable[randomIndex]}")
+                }
+            }
+        }
+    }
+}
